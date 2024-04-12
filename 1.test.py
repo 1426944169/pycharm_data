231c2e8d6dc7b0eb3afbb81ff28467437277f6f6
niushibo <1426944169@qq.com>
2024-04-12 11:01:49 +0800

    第一次Changes

---
+++ 1.test.py
@@ -0,0 +1,26 @@
+﻿# 连接momgdb数据库和mysql数据库
+import pymongo
+
+import pymysql
+mongo = pymongo.MongoClient(host='localhost', port=27017)
+db = mongo['test']
+collection = db['test']
+mysql = pymysql.connect(host='localhost', user='root', password='root', port=3306, db='test')
+# 创建游标
+cursor = mysql.cursor()
+
+
+for i in range(1,10):
+    print(i)
+    # mysql插入数据
+    sql = "insert into test(i) values(%s)"
+    cursor.execute(sql, (i))
+    mysql.commit()
+cursor.close()
+mysql.close()
+print('插入成功')
+
+
+# cursor.close()
+# mysql.close()
+
